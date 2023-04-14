3e39c45416f98a0bd655fcc2e176e72f641390bc
DubsG <gichuhigeorge041@gmail.com>
2023-04-14 09:10:02 -0700

    My static library

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -2,9 +2,9 @@
 #include <stddef.h>
 
 /**
- * _strchr - Function that finds first occurence of a character
- * @s: input character
- * @c: input character
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
