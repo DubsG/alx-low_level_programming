c9689ec26312fb76812f611b54b11d73bc1c3ae5
DubsG <gichuhigeorge041@gmail.com>
2023-04-14 08:48:31 -0700

    Coding for my static library.

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - concatenates(links) two strings
+ * @dest: string to be linked
+ * @src: string to be linked
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - calcualtes the absolute value of an integer
+ * @n: the integer to be checked
+ * Return: the absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
