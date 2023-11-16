bc00e8e559c2608ec86edb0e7a8970a0f88c3d96
DubsG <gichuhigeorge041@gmail.com>
2023-11-16 00:20:36 +0000

    Recursion function to print a string in a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
-  *_puts_recursion -the main fucntion
-  *
-  * @s: function parameter
-  *
-  * Return: 0 always.
-  */
-void _puts_recursion(char *s);
+ *   * _puts_recursion - main function
+ *     *
+ *       * @s: function parameter
+ *         *
+ *           * Return: Always 0.
+ *             */
+void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	}
-	else
-	_putchar('\n');
+		if (*s)
+				{
+						_putchar(*s);
+							_puts_recursion(s + 1);
+								}
+			else
+					_putchar('\n');
 }
+
 
